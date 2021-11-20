985c7b78d71d773d80137618d329d5e88103341d
detrasmental <detra.gordon76@gmail.com>
2021-11-20 15:43:55 -0500

    defined columns in the models folder

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,12 +8,26 @@ ProductTag.init(
   {
     // define columns
     id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
     },
     tag_id: {
-  }
-},
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
+  },
   {
     sequelize,
     timestamps: false,
